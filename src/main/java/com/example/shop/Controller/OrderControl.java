a58b089874c1c0dde978f9dce0e99ffb7ff49e14
Jana-2002-143 <janaganl1011@gmail.com>
2025-12-06 10:31:05 +0530

    changes

--- src/main/java/com/example/shop/Controller/OrderControl.java
+++ src/main/java/com/example/shop/Controller/OrderControl.java
@@ -1,7 +1,6 @@
 package com.example.shop.Controller;
 
 import com.example.shop.Entity.Order;
-import com.example.shop.Entity.User;
 import com.example.shop.Repository.OrderRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
